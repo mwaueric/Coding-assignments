73ff987387099d40c8fd7618d48873cdcfa8e7d0
MwauEric-CT102/G/21960/24 <ericombithi289@gmail.com>
2024-10-09 16:27:54 +0300

    Create Loan eligibility.c

---
+++ Loan eligibility.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	int age;
+	float income;
+	
+	//Input age and income from the user
+	printf("Enter your age:");
+	scanf("%d",&age);
+	
+	printf("Enter your annual income:");
+	scanf("%f",&income);
+	
+	//Check if the customer is eligible for a loan
+	if (age>=21&&income>=21000){
+		printf("Congratulations you qualify for a loan.\n");
+	}else{
+		printf("Unfortunately,we are unable to offer you a loan at this time.\n");
+	}
+	
+	return 0;
+}
