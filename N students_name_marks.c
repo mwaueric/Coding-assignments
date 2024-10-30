e665c9f3b90d1f531fff9840971fb6256598edbb
MwauEric-CT102/G/21960/24 <ericombithi289@gmail.com>
2024-10-30 18:07:54 +0300

    Create N students_name_marks.c

---
+++ N students_name_marks.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int marks;
+};
+
+int main() {
+    int n;
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    FILE *file = fopen("student_records.txt", "a");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    struct Student student;
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter name of student %d: ", i+1);
+        scanf("%s", student.name);
+        printf("Enter marks of student %d: ", i+1);
+        scanf("%d", &student.marks);
+
+        fprintf(file, "%s %d\n", student.name, student.marks);
+    }
+
+    fclose(file);
+    printf("Data appended to students_records.txt successfully.\n");
+
+    return 0;
+}
